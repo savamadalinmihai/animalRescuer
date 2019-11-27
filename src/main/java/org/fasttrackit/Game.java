5d5a8bd972b9d3a94232a89f32a58e0a524c1bcb
savamadalinmihai <savamadalinmihai@gmail.com>
2019-11-27 10:03:49 +0200

    Tema, pus constructorii in Game.

--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,8 +2,19 @@ package org.fasttrackit;
 
 // Aici vom pune variabilele jocului, cu if statements, when, etc.
 
+
+
 public class Game {
 
+    public Game(String name, int durationInMinutes, boolean toysRequired) {
+        this.name = name;
+        this.durationInMinutes = durationInMinutes;
+        this.toysRequired = toysRequired;
+    }
+
+    String name;
+    int durationInMinutes;
+    boolean toysRequired;
 
 
 }
